675c0d02533ec3b4e168024d6f78c3fd8523c4ec
Lucas Monteiro <lucas.monteiro@havas.com>
2021-07-12 13:35:12 -0300

    first commit

---
+++ olympic/spiders/contries_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class ScrapeTableSpider(scrapy.Spider):
+    name = 'contries'
+
+    def start_requests(self):
+        urls = [
+            'https://www.whereig.com/olympics/medals/tokyo-2020-21-olympics-medal-table.html',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for row in response.xpath('/html/body/div/div[2]/main/section/div[2]/table[3]//tbody/tr'):
+            yield {
+                'country': row.xpath('td[1]//text()').extract_first(),
+                'gold': row.xpath('td[2]//text()').extract_first(),
+                'silver': row.xpath('td[3]//text()').extract_first(),
+                'bronze': row.xpath('td[4]//text()').extract_first(),
+                'total': row.xpath('td[5]//text()').extract_first(),
+            }
---
+++ readme.md
@@ -0,0 +1,17 @@
+# About
+
+A web scraping in python using scrapy to get info about countries in Tokyo 2020 Olympic
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install scrapy.
+
+```bash
+pip install scrapy
+```
+
+## Usage
+
+Run
+```python
+scrapy crawl --nolog --output -:json contries
