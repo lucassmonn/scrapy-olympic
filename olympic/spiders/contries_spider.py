9081c01e29087a216a7041edd3cdef2510387c41
Lucas Monteiro <lucas.monteiro@havas.com>
2021-07-24 15:44:14 -0300

    ajust

--- olympic/spiders/contries_spider.py
+++ olympic/spiders/contries_spider.py
@@ -15,9 +15,9 @@ class ScrapeTableSpider(scrapy.Spider):
         for row in response.xpath('/html/body/div/div[2]/main/section/div[2]/table[2]/tbody[1]/tr'):
             yield {
                 'place': row.xpath('td[1]//text()').extract_first(),
-                'country': row.xpath('td[1]//text()').extract_first(),
-                'gold': row.xpath('td[2]//text()').extract_first(),
-                'silver': row.xpath('td[3]//text()').extract_first(),
-                'bronze': row.xpath('td[4]//text()').extract_first(),
-                'total': row.xpath('td[5]//text()').extract_first(),
+                'country': row.xpath('td[2]//text()').extract_first(),
+                'gold': row.xpath('td[3]//text()').extract_first(),
+                'silver': row.xpath('td[4]//text()').extract_first(),
+                'bronze': row.xpath('td[5]//text()').extract_first(),
+                'total': row.xpath('td[6]//text()').extract_first(),
             }
